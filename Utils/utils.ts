ab7856d67397308e0588525ab11e95da6c7060b0
Silence <13250487+Silence-kzp@users.noreply.github.com>
2020-03-20 14:54:11 +0800

    Update utils.ts

--- Utils/utils.ts
+++ Utils/utils.ts
@@ -142,3 +142,18 @@ export function VerifyIDCard(idcard: string) {
     return ids[mod] === idcard[17];
 }
 
+/**
+ *  随机生成key
+ *  
+ *  @param length 长度
+ */
+export function RandomKey(length = 6) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
+
